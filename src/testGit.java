4203c7b794cafb0236555ce978525a2fe2d7addc
mainhatthanh <thanh240205@gmail.com>
2024-09-26 10:56:45 +0700

    Update testGit.java

--- src/testGit.java
+++ src/testGit.java
@@ -1,6 +1,6 @@
 public class testGit {
     public static void main(String[] args) {
-        System.out.println("ocChoQuang");
-        System.out.println("ocLonQuang");
+        System.out.println("ocCuQuang");
+      
     }
 }
